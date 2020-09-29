2caefef7f1be4c490d9ce46895ad1d9f388ef1f7
fndsz <45010248+fndsz@users.noreply.github.com>
2020-09-29 10:02:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,9 +27,6 @@ sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 echo "修改时区"
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
 
-echo '修改banner'
-rm -rf package/base-files/files/etc/banner
-cp -f ../banner package/base-files/files/etc/
 
 echo "修改wifi名称"
 
